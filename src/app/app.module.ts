1e1661bd2b90ff34933c6bddbc982bc287685a89
tarek.boufar@univ-constantine2.dz <tarek.boufar@univ-constantine2.dz>
2023-01-07 04:16:39 +0100

    Before updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { TableDataComponent } from './table-data/table-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableDataComponent
   ],
   imports: [
     BrowserModule,
