7a4eeb244a03dd4de6f51a45eacf07bf915765af
tarek.boufar@univ-constantine2.dz <tarek.boufar@univ-constantine2.dz>
2023-01-07 13:30:41 +0100

    Get data from Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
@@ -10,6 +9,8 @@ import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { TableDataComponent } from './table-data/table-data.component';
 import {MatTableModule} from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/table-data/table-data.component.ts
+++ src/app/table-data/table-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SpectralColor } from './SpectralColor';
-
+import { map} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-table-data',
@@ -9,43 +10,32 @@ import { SpectralColor } from './SpectralColor';
 })
 export class TableDataComponent implements OnInit {
 
-  Data: SpectralColor[] = [{
-    "Id": 1,
-    "Date": "06/11",
-    "F1": 21,
-    "F2": 19,
-    "F3": 21,
-    "F4": 76,
-    "F5": 205,
-    "F6": 98,
-    "F7": 39,
-    "F8": 71,
-    "NIR_1": 68,
-    "NIR_2": 54,
-    "Ripeness": 0
-  },
-  {
-    "Id": 2,
-    "Date" : "06/12",
-    "F1": 18,
-    "F2": 17,
-    "F3": 18,
-    "F4": 69,
-    "F5": 191,
-    "F6": 93,
-    "F7": 39,
-    "F8": 71,
-    "NIR_1": 62,
-    "NIR_2": 58,
-    "Ripeness": 0
-  }
-  ];
+  public train_SpectralColors : any[] = [];
+  public mesured_SpectralColors : SpectralColor[] = [];
 
   displayedColumns: string[] = ["Id", "Date", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "NIR_1", "NIR_2", "Ripeness"];
+  public url = 'https://indicateur-de-maturite-2d30f-default-rtdb.europe-west1.firebasedatabase.app/'
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.fetchMesured_SpectralColors();
   }
 
+  private fetchMesured_SpectralColors(){
+    this.http.get<{[key : string]:SpectralColor}>(this.url+'DataTrain.json')
+    .pipe(map((res)=>{
+      const spectralColors = []
+      for (const key in res){
+        if (res.hasOwnProperty(key)){
+          spectralColors.push({...res[key], Id : key})
+        }
+      }
+      return spectralColors;
+    }))
+    .subscribe((spectralColors) => {
+      console.log(spectralColors);
+      this.mesured_SpectralColors = spectralColors;
+    })
+  }
 }
